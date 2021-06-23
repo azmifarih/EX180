d3a8646efce725d200da0e145bc6ecb3b5897cd4
mascolo99 <mascolo99@gmail.com>
2021-06-23 09:19:25 -0300

    fix start p2

--- P2/start.sh
+++ P2/start.sh
@@ -8,5 +8,5 @@ sudo podman build -t do180-p2 .
 #restorecon -Rv info
 #ls -ldZ
 
-sudo podman run -d  --name do180-p2c  -v /var/moscodb:/var/lib/mysql/data -p 13306:3306 -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_DATABASE=do180 -e MYSQL_ROOT_PASSWORD=r00tpa5 do180-p2
+sudo podman run -d  --name do180-p2c  -v /var/mosco:/var/lib/mysql/data -p 13306:3306 -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_DATABASE=do180 -e MYSQL_ROOT_PASSWORD=r00tpa5 do180-p2
 #sudo podman run -d  --name do180-p21 -v ${PWD}/mysql:/opt/app-root/src:Z -p 13306:3306 -e MYSQL_USER=user1 -e MYSQL_PASSWORD=mypa55 -e MYSQL_DATABASE=items -e MYSQL_ROOT_PASSWORD=r00tpa5 rhscl/mysql-57-rhel7
